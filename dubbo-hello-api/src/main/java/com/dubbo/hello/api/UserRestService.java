71a57a5a54b80b6f88851aa7d91949b0ad527ffa
liwenyi <liwenyi@bluemoon.com.cn>
2016-07-26 00:01:42 +0800

    delete test hello

--- dubbo-hello-api/src/main/java/com/dubbo/hello/api/UserRestService.java
+++
@@ -1,5 +0,0 @@
-package com.dubbo.hello.api;
-import javax.validation.constraints.Min;
-public interface UserRestService {
-	User getUser(@Min(value = 1L, message = "User ID must be greater than 1") Long id);
-}
--- dubbo-hello-api/src/main/java/com/dubbo/hello/api/UserService.java
+++
@@ -1,5 +0,0 @@
-package com.dubbo.hello.api;
-
-public interface UserService {
-	User getUser(Long id);
-}
